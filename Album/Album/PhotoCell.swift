88931d730850b95fe0b49d642360ef840019a639
zorua14 <94368303+zorua14@users.noreply.github.com>
2023-09-22 17:29:58 +0530

    Add files via upload

---
+++ Album/Album/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  Album
+//
+//  Created by Karthikeyan Muthu on 22/09/23.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+}
